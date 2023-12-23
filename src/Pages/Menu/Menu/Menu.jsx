9c35e4283fefa3b9591529c1fae6f12d763f8995
Yousufjoy <kanekiken333@gmail.com>
2023-12-23 18:17:41 +0600

    Dynamic Page Title By Route Using React Helmet

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet-async";
+
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Flair_Feast | Menu</title>
+      </Helmet>
+    </div>
+  );
+};
+
+export default Menu;
