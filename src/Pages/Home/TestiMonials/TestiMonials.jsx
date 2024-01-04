bbff62096d5ef197cb51e6612c919da6c2d81788
Yousufjoy <kanekiken333@gmail.com>
2024-01-04 14:41:18 +0600

    login form added

--- src/Pages/Home/TestiMonials/TestiMonials.jsx
+++ src/Pages/Home/TestiMonials/TestiMonials.jsx
@@ -13,7 +13,7 @@ const TestiMonials = () => {
   const [reviwes, setReviews] = useState([]);
 
   useEffect(() => {
-    fetch("reviews.json")
+    fetch("http://localhost:5000/reviews")
       .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);
