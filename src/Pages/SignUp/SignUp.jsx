d19fec1af84d4b165b62ed74a242fed807939d2d
yousufjoy <yousufj93006@gmail.com>
2024-01-11 20:31:07 +0600

    Regex Validation In React Hook Form And Signup

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,31 +1,41 @@
+import { useContext } from "react";
+import { Helmet } from "react-helmet";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-
-
+import { AuthContext } from "../../providers/AuthProvider";
 
 const SignUp = () => {
-
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = data =>{
-    console.log("The data",data);
-  }
- 
-   
-  
+  const {createUser} = useContext(AuthContext);
+
+  const onSubmit = (data) => {
+    console.log("The data", data);
+    createUser(data.email, data.password)
+    .then (result => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+    })
+
+  };
+
   return (
-        <div className="hero min-h-screen bg-base-200">
+    <>
+      <Helmet>
+        <title>Flair_Feast | Sign Up</title>
+      </Helmet>
+      <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col md:flex-row-reverse">
           <div className="text-center md:w-1/2lg:text-left">
             <h1 className="text-5xl font-bold">Sign Up now!</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-              a id nisi.
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
             </p>
           </div>
           <div className="card md:w-1/2 shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -36,48 +46,64 @@ const SignUp = () => {
                 </label>
                 <input
                   type="text"
-                  {...register("name",{ required: true })}
+                  {...register("name", { required: true })}
                   name="name"
                   placeholder="name"
                   className="input input-bordered"
-              
                 />
-                {errors.name && <span className="text-red-600" >This field is required</span>}
+                {errors.name && (
+                  <span className="text-red-600">This field is required</span>
+                )}
               </div>
               <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                {...register("email",{ required: true })}
-                name="email"
-                placeholder="email"
-                className="input input-bordered"
-               
-              />
-              {errors.email && <span className="text-red-600" >This field is required</span>}
-            </div>
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  {...register("email", { required: true })}
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+                {errors.email && (
+                  <span className="text-red-600">This field is required</span>
+                )}
+              </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
                 <input
                   type="password"
-                  {...register("password",{ required: true, minLength: 6, maxLength: 20 })}
-                  name="password"
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                  })}
                   placeholder="password"
-                  className="input input-bordered "
-                 
+                  className="input input-bordered"
                 />
-                {errors.password && <span className="text-red-600">Pssword is required</span>}
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
+                {errors.password?.type === "required" && (
+                  <p className="text-red-600">Password is required</p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-600">Password must be 6 characters</p>
+                )}
+                {errors.password?.type === "maxLength" && (
+                  <p className="text-red-600">
+                    Password must be less than 20 characters
+                  </p>
+                )}
+                {errors.password?.type === "pattern" && (
+                  <p className="text-red-600">
+                    Password must have one Uppercase one lower case, one number
+                    and one special character.
+                  </p>
+                )}
               </div>
-            
+
               <div className="form-control mt-6">
                 <input
                   className="btn btn-primary"
@@ -86,12 +112,16 @@ const SignUp = () => {
                 />
               </div>
             </form>
-           <p> <small> Already have an account? </small> <Link to="/login">Login</Link>  </p>
+            <p>
+              {" "}
+              <small> Already have an account? </small>{" "}
+              <Link to="/login">Login</Link>{" "}
+            </p>
           </div>
-      
         </div>
       </div>
-    );
+    </>
+  );
 };
 
 export default SignUp;
