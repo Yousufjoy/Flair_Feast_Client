14fe7c0a056ed26fbddfa7b7a615e4c3f514f2ce
yousufjoy <yousufj93006@gmail.com>
2024-03-14 17:49:45 +0600

    Display All Users And Create Make Admin API

--- src/LayOut/Dashboard.jsx
+++ src/LayOut/Dashboard.jsx
@@ -8,39 +8,81 @@ import {
   FaList,
   FaSearch,
 } from "react-icons/fa";
+import { MdRecordVoiceOver,MdOutlineManageSearch  } from "react-icons/md";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { TbBrandBooking } from "react-icons/tb";
 
 const Dashboard = () => {
   const [cart] = useCart();
+
+  // TODO: get isAdmin value from the database
+  const isAdmin = true;
+
   return (
     <div className="flex">
       {/* dashboard sidebar */}
       <div className="w-64 min-h-screen bg-orange-400">
         <ul className="menu p-4">
-          <li>
-            <NavLink to="/dashboard/cart">
-              <FaShoppingCart></FaShoppingCart>My Cart ({cart.length})
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/userHome">
-              <FaHome></FaHome>user Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/reservation">
-              <FaCalendar></FaCalendar>Reservation
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/review">
-              <FaAd></FaAd>Review
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/bookings">
-              <FaList></FaList>My Bookings
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+           
+              <li>
+                <NavLink to="/dashboard/adminHome">
+                  <FaHome></FaHome>Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/addItems">
+                <IoIosAddCircleOutline />Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageItems">
+                <MdOutlineManageSearch />Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/bookings">
+                <TbBrandBooking />Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/users">
+                  <FaList></FaList>All Users
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/cart">
+                  <FaShoppingCart></FaShoppingCart>My Cart ({cart.length})
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/userHome">
+                  <FaHome></FaHome>user Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservation">
+                  <FaCalendar></FaCalendar>Reservation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/review">
+                  <FaAd></FaAd>Review
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/bookings">
+                  <FaList></FaList>My Bookings
+                </NavLink>
+              </li>
+            </>
+          )}
+          {/* shared nav links */}
+
           <div className="divider"></div>
           <li>
             <NavLink to="/">
@@ -48,8 +90,13 @@ const Dashboard = () => {
             </NavLink>
           </li>
           <li>
-            <NavLink to="/">
+            <NavLink to="/order/salad">
               <FaSearch></FaSearch>Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/contact">
+              <MdRecordVoiceOver /> Contact
             </NavLink>
           </li>
         </ul>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./PrivateRoute";
 import Secret from "../Pages/Shared/Secret";
 import Dashboard from "./../LayOut/Dashboard";
 import Cart from "./../Pages/Dashboard/Cart/Cart";
+import AllUsers from './../Pages/Dashboard/Cart/AllUsers/AllUsers';
 
 export const router = createBrowserRouter([
   {
@@ -57,6 +58,11 @@ export const router = createBrowserRouter([
         path: "cart",
         element: <Cart></Cart>,
       },
+      // admin routes
+      {
+        path: "users",
+        element: <AllUsers></AllUsers>
+      }
     ],
   },
 ]);
